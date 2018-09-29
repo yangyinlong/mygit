3f6a8698af720421aeacdee65f6d1e6b98f96a07
yyl <yyl@163.com>
2018-09-29 20:05:56 +0800

    添加test class

---
+++ mygit/src/main/java/com/itheima/rua/hello.java
@@ -0,0 +1,9 @@
+package com.itheima.rua;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("bboom!! bboom!! bboom!!");
+        System.out.println("bboom!! bboom!! bboom!!");
+        System.out.println("bboom!! bboom!! bboom!!");
+    }
+}
